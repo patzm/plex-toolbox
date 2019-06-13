04025066b644d69f23aad7082fad9a20008784ef
Martin Patz <mailto@martin-patz.de>
2019-06-13 14:18:35 -0700

    adds a minimal setup routine

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="plex-scripts",
+    version="0.1",
+    packages=[],
+    scripts=['download-album'],
+    install_requires=['absl-py', 'pathvalidate', 'plexapi', 'tqdm'],
+    author="Martin Patz",
+    author_email="mailto@martin-patz.de",
+    description="A loose collection of scripts to interact with "
+                "a Plex Media Server (PMS).",
+    license="GNU GENERAL PUBLIC LICENSE",
+    license_file="LICENSE",
+)

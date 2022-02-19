8d37ca81afdb02ce9ec6a9a180eb17fcc85401cd
Martin Patz <mailto@martin-patz.de>
2022-02-19 15:46:29 +0100

    black formatting

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name="plex-toolbox",
     version="0.1",
     packages=[],
-    scripts=['scripts/download-album'],
-    install_requires=['absl-py', 'pathvalidate', 'plexapi', 'tqdm'],
+    scripts=["scripts/download-album"],
+    install_requires=["absl-py", "pathvalidate", "plexapi", "tqdm"],
     author="Martin Patz",
     author_email="mailto@martin-patz.de",
     description="A loose collection of scripts to interact with "
-                "a Plex Media Server (PMS).",
+    "a Plex Media Server (PMS).",
     license="GNU GENERAL PUBLIC LICENSE",
     license_file="LICENSE",
 )
